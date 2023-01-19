e1c73a9a54f0e76b75f2efb1db3add1ef4a0d241
youz17 <youz17@qq.com>
2023-01-19 23:41:31 +0800

    Course.Optional: finished

--- src/Course/Optional.hs
+++ src/Course/Optional.hs
@@ -1,18 +1,18 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Course.Optional where
 
-import qualified Control.Applicative as A
-import qualified Control.Monad as M
+import Control.Applicative qualified as A
+import Control.Monad qualified as M
 import Course.Core
-import qualified Prelude as P
+import Prelude qualified as P
 
 -- | The `Optional` data type contains 0 or 1 value.
 --
 -- It might be thought of as a list, with a maximum length of one.
-data Optional a =
-  Full a
+data Optional a
+  = Full a
   | Empty
   deriving (Eq, Show)
 
@@ -24,11 +24,11 @@ data Optional a =
 -- >>> mapOptional (+1) (Full 8)
 -- Full 9
 mapOptional ::
-  (a -> b)
-  -> Optional a
-  -> Optional b
-mapOptional =
-  error "todo: Course.Optional#mapOptional"
+  (a -> b) ->
+  Optional a ->
+  Optional b
+mapOptional f (Full a) = Full (f a)
+mapOptional _ _ = Empty
 
 -- | Bind the given function on the possible value.
 --
@@ -41,11 +41,11 @@ mapOptional =
 -- >>> bindOptional (\n -> if even n then Full (n - 1) else Full (n + 1)) (Full 9)
 -- Full 10
 bindOptional ::
-  (a -> Optional b)
-  -> Optional a
-  -> Optional b
-bindOptional =
-  error "todo: Course.Optional#bindOptional"
+  (a -> Optional b) ->
+  Optional a ->
+  Optional b
+bindOptional f (Full a) = f a
+bindOptional _ _ = Empty
 
 -- | Return the possible value if it exists; otherwise, the second argument.
 --
@@ -55,11 +55,11 @@ bindOptional =
 -- >>> Empty ?? 99
 -- 99
 (??) ::
-  Optional a
-  -> a
-  -> a
-(??) =
-  error "todo: Course.Optional#(??)"
+  Optional a ->
+  a ->
+  a
+(??) Empty b = b
+(??) (Full a) _ = a
 
 -- | Try the first optional for a value. If it has a value, use it; otherwise,
 -- use the second value.
@@ -76,11 +76,12 @@ bindOptional =
 -- >>> Empty <+> Empty
 -- Empty
 (<+>) ::
+  Optional a ->
+  Optional a ->
   Optional a
-  -> Optional a
-  -> Optional a
-(<+>) =
-  error "todo: Course.Optional#(<+>)"  
+(<+>) fa@(Full _) _ = fa
+(<+>) Empty fb@(Full _) = fb
+(<+>) Empty Empty = Empty
 
 applyOptional :: Optional (a -> b) -> Optional a -> Optional b
 applyOptional f a = bindOptional (\f' -> mapOptional f' a) f
