77d3ec0d0f8af49132df2a78b9b0aee16c960721
youz17 <youz17@qq.com>
2023-01-21 19:05:48 +0800

    FileIO: finished

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Course.FileIO where
 
-import Course.Core
 import Course.Applicative
-import Course.Monad
+import Course.Core
 import Course.Functor
 import Course.List
+import Course.Monad
+import GHC.Read (readField)
 
 {-
 
@@ -39,7 +40,7 @@ Problem --
 
 Consideration --
   Try to avoid repetition. Factor out any common expressions.
-  
+
 Example --
 Given file files.txt, containing:
   a.txt
@@ -60,8 +61,8 @@ To test this module, load ghci in the root of the project directory, and do
 
 Example output:
 
-$ ghci
-GHCi, version ... 
+\$ ghci
+GHCi, version ...
 Loading package...
 Loading ...
 [ 1 of 28] Compiling (etc...
@@ -82,49 +83,58 @@ the contents of c
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
 printFile ::
-  FilePath
-  -> Chars
-  -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+  FilePath ->
+  Chars ->
+  IO ()
+printFile path str =
+  putStrLn path
+    >> putStrLn str
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
-  List (FilePath, Chars)
-  -> IO ()
+  List (FilePath, Chars) ->
+  IO ()
 printFiles =
-  error "todo: Course.FileIO#printFiles"
+  foldLeft (\x y -> x >> uncurry printFile y) (pure ())
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
-  FilePath
-  -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+  FilePath ->
+  IO (FilePath, Chars)
+getFile path = nameAndContent <$> readFile path
+  where
+    nameAndContent content = (path, content)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
-  List FilePath
-  -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+  List FilePath ->
+  IO (List (FilePath, Chars))
+getFiles pathList = sequence $ getFile <$> pathList
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
-  FilePath
-  -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+  FilePath ->
+  IO ()
+run path = do
+  content <- readFile path
+  let files = lines content
+  getFiles files >>= printFiles
+  return ()
+
+head :: List a -> a
+head (x :. _) = x
+head Nil = error "need argument"
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  args <- getArgs
+  run $ head args
 
 ----
 
